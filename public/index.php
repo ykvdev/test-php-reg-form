37129813d9a3345c4f54c7c225e648b62e80beaf
atoumus <atoumus@gmail.com>
2019-11-03 11:56:12 +0200

    Add favicon

--- public/index.php
+++ public/index.php
@@ -12,10 +12,13 @@ $env = in_array($env, [ENV_DEV, ENV_PROD]) ? $env : ENV_DEV;
 define('APP_ENV', $env);
 
 // ONLY FOR PHP BUILT-IN SERVER...
-if(APP_ENV == ENV_DEV && $_SERVER['REQUEST_URI'] == '/assets/css/common.css') {
+if(APP_ENV == ENV_DEV && in_array($_SERVER['REQUEST_URI'], [
+    '/assets/css/common.css',
+    '/favicon.ico'
+], true)) {
     header("Content-Type: text/css");
     header("X-Content-Type-Options: nosniff");
-    echo file_get_contents(__DIR__ . '/assets/css/common.css');
+    echo file_get_contents(__DIR__ . $_SERVER['REQUEST_URI']);
     exit;
 }
 
